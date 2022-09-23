702f796707d1f9f7027ec28cb8589b7435ea7234
mijihime <mjkwz@naver.com>
2022-09-23 18:33:49 +0900

    0923

---
+++ spring.core.22.10/src/main/java/spring/core/ch01/ex05/Pistol.java
@@ -0,0 +1,12 @@
+//2022-09-23 - 3단계 @Component
+package spring.core.ch01.ex05;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class Pistol implements Gun{
+	@Override
+	public void fire() {
+		System.out.println("권총 탕탕.");
+	}
+}
